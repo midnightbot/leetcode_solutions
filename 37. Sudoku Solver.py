2ff32da2f26116b7c9ec3eb1d0328651e67c264b
notunknown <44056065+midnightbot@users.noreply.github.com>
2021-02-23 09:01:01 +0530

    Create 37. Sudoku Solver.py

---
+++ 37. Sudoku Solver.py
@@ -0,0 +1,25 @@
+class Solution:
+    def solveSudoku(self, board):
+        def isValid(x,y):
+            tmp=board[x][y]; board[x][y]='D'
+            for i in range(9):
+                if board[i][y]==tmp: return False
+            for i in range(9):
+                if board[x][i]==tmp: return False
+            for i in range(3):
+                for j in range(3):
+                    if board[int(x/3)*3+i][int(y/3)*3+j]==tmp: return False
+            board[x][y]=tmp
+            return True
+        def dfs(board):
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j]=='.':
+                        for k in '123456789':
+                            board[i][j]=k
+                            if isValid(i,j) and dfs(board):
+                                return True
+                            board[i][j]='.'
+                        return False
+            return True
+        dfs(board)
