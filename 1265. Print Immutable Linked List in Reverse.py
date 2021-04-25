02151db148362e0b572200ee159c890e1c36a192
notunknown <44056065+midnightbot@users.noreply.github.com>
2021-04-25 07:26:42 +0530

    Create 1265. Print Immutable Linked List in Reverse.py

---
+++ 1265. Print Immutable Linked List in Reverse.py
@@ -0,0 +1,19 @@
+# """
+# This is the ImmutableListNode's API interface.
+# You should not implement it, or speculate about its implementation.
+# """
+# class ImmutableListNode:
+#     def printValue(self) -> None: # print the value of this node.
+#     def getNext(self) -> 'ImmutableListNode': # return the next node.
+
+class Solution:
+    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
+        
+        if head.getNext():
+            self.printLinkedListInReverse(head.getNext())
+            
+        head.printValue()
+            
+            
+       
+        
