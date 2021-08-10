3b1ef557ea0431b431ba04b0678fba8ab8125315
notunknown <44056065+midnightbot@users.noreply.github.com>
2021-08-10 15:21:20 -0700

    Create 1965. Employees With Missing Information.sql

---
+++ 1965. Employees With Missing Information.sql
@@ -0,0 +1,5 @@
+SELECT employee_id FROM Employees WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
+UNION 
+SELECT employee_id FROM Salaries WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+
+ORDER BY 1 ASC
