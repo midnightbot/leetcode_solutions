7ba4ca353d11b0892a86719952dd1e15e1512cd9
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2021-11-14 17:56:54 -0800

    Update 368. Largest Divisible Subset.py

--- 368. Largest Divisible Subset.py
+++ 368. Largest Divisible Subset.py
@@ -1,4 +1,4 @@
-(Approach 1 : Time Limit Exceeded 27/48 Test Cases Passed)
+##(Approach 1 : Time Limit Exceeded 27/48 Test Cases Passed)
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         
@@ -36,8 +36,35 @@ class Solution:
                 
         return (rets[::-1])
             
-            
-            
+################################################################################################################################################################          
+## Approach 2 (Passed)
+##Similar to Approach1, the only change being rather than maintainting a 2-D dp array, created a 1-D dp array
+##2-D dp array gives max subset for every set starting from i and ending at j
+## But that is not necessary, instead we can have 1-D array and see if the element i is in the max subset or not
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        
+        nums = sorted(nums)
+        
+        dp  = [1 for x in range(len(nums))]
+        
+        for x in range(1,len(dp)):
+            for y in range(x):
+                if nums[x]%nums[y] == 0:
+                    dp[x] = max(dp[x],1 + dp[y])
+                    
+        index = dp.index(max(dp))
+        print(dp)
+        ans  = []
+        ans.append(nums[index])
+        current = max(dp)
+        for x in range(index-1,-1,-1):
+            if dp[x]+1 == current and ans[len(ans)-1]%nums[x] == 0:
+                ans.append(nums[x])
+                current = dp[x]
+                
+        return ans[::-1]
+                
             
         
             
