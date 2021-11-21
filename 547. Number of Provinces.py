e1804d8f9958da5acc2f4e98f07a081ba0c555a2
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2021-11-21 10:16:24 -0800

    Create 547. Number of Provinces.py

---
+++ 547. Number of Provinces.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+        counter = 0
+        
+        queue = set()
+        
+        for x in range(len(isConnected)):
+            for y in range(len(isConnected[0])):
+                if isConnected[x][y] == 1:
+                    queue.add(tuple((x,y)))
+                    counter+=1
+                    while queue:
+                        
+                        node = queue.pop()
+                        isConnected[node[0]][node[1]] = -1
+                        for z in range(len(isConnected[0])):
+                            if isConnected[node[1]][z] == 1:
+                                queue.add(tuple((node[1],z)))
+                            
+        return counter
