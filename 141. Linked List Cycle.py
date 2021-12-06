96c22a82112378ca90c78f887a13935bb0ff345f
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2021-12-06 12:24:45 -0800

    Update 141. Linked List Cycle.py

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -22,3 +22,28 @@ class Solution:
             
         return False
         
+##Solution 2
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        if head == None:
+            return False
+        
+        for x in range(pow(10,4)+1):
+            if head.next!=None:
+                head = head.next
+                
+            else:
+                return False
+            
+        return True
+            
+            
+        
+        
