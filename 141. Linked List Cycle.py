395c84924d1d791de6b79cd291d90a66d2fe6b67
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2021-12-06 13:05:19 -0800

    Update 141. Linked List Cycle.py

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -46,4 +46,31 @@ class Solution:
             
             
         
+##Solution 3 (Classic hare and tortoise problem)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        if head == None or head.next == None:
+            return False
+        
+        tortoise = head
+        hare = head.next
+        
+        while hare and hare.next:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if tortoise == hare:
+                return True
+            
+        return False
+        
+        
+            
+        
         
