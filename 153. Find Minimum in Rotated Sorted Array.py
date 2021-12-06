8eb4f4907ca8fbc49e3edae98a8e72a6e2b17cf3
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2021-12-05 16:51:40 -0800

    Create 153. Find Minimum in Rotated Sorted Array.py

---
+++ 153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,18 @@
+##ss
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        left = 0
+        right = len(nums) -  1
+        
+        while left < right:
+            mid = left +(right - left) // 2
+            
+            if nums[mid] < nums[right]:
+                right = mid
+                
+            else:
+                left = mid + 1
+                
+        return nums[left]
+        
