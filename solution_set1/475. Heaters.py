6a8363e79dcbbc412f1a3685a36974b45850586d
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2022-01-07 14:39:00 -0800

    Update 475. Heaters.py

--- solution_set1/475. Heaters.py
+++ solution_set1/475. Heaters.py
@@ -1,4 +1,5 @@
 ##ss
+##Solution 1 
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         
@@ -23,6 +24,36 @@ class Solution:
                     left = mid + 1
                     
         return max(result)
+                
+        
+        
+##Solution 2 (Same as Solution 1 but without creating result array)
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        
+        #result = [float('inf') for x in range(len(houses))]
+        finalans = -float('inf')
+        houses = sorted(houses)
+        heaters = sorted(heaters)
+        for x in range(len(houses)):
+            
+            left = 0
+            right = len(heaters) - 1
+            thisans = float('inf')
+            while left <= right:
+                mid = left +(right - left) // 2
+                
+                if abs(heaters[mid] - houses[x]) < thisans:
+                    thisans = abs(heaters[mid] - houses[x])
+                    
+                if heaters[mid] > houses[x]:
+                    right = mid - 1
+                    
+                else:
+                    left = mid + 1
+                    
+            finalans = max(finalans,thisans)
+        return finalans
                 
         
         
