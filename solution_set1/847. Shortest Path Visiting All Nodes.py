20939de695d199260d46ae8e697b67958cae88b5
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2022-02-25 23:30:32 -0800

    Update 847. Shortest Path Visiting All Nodes.py

--- solution_set1/847. Shortest Path Visiting All Nodes.py
+++ solution_set1/847. Shortest Path Visiting All Nodes.py
@@ -52,4 +52,53 @@ class Solution:
                         
         #print("_______")
         return float('inf')
+     
         
+  ##Solution 2(Accepted)
+
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        
+        anss = float('inf')
+        ##use bitmask to represent visited nodes
+        
+        n = len(graph)
+        
+        dist = [[float('inf') for x in range(n)]for y in range(n)]
+        
+        for i,edges in enumerate(graph):
+            dist[i][i] = 0
+            for edge in edges:
+                dist[i][edge] = 1
+                
+        ##floyd's warshalls
+        
+        for x in range(n):
+            for y in range(n):
+                for z in range(n):
+                    dist[y][z] = min(dist[y][z], dist[y][x] + dist[x][z])
+                    
+        @lru_cache(None)            
+        def visit(node,visited):
+            ##if all nodes visited, say three nodes 111
+            ## 111 - 1 = 100
+            if visited == (1 << n) - 1:##checking if all nodes are visited
+                return 0
+            
+            ans = float('inf')
+            
+            for x in range(n):
+                
+                #if str(visited)[x] == '0':
+                if ((1<<x) & (visited)) == 0:##if this node 'x' is not visited, visit it next
+                    ans = min(ans, visit(x,(1<<x) | visited) + dist[node][x])
+                    
+                    
+            return ans
+        
+        #print(dist)
+        for x in range(n):
+            anss = min(anss, visit(x,(1<<x)))
+            
+        return anss
+                
