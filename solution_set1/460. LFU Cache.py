804ef760b688f2e96dc2a1df6828e6ec59a80b4a
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2022-02-27 18:03:27 -0800

    Update 460. LFU Cache.py

--- solution_set1/460. LFU Cache.py
+++ solution_set1/460. LFU Cache.py
@@ -1,4 +1,71 @@
 ##ss
+##Solution1 (Time Limit Exceeded 23/25 Test cases passed)
+class LFUCache:
+    ##least freq and then least recently used
+    
+    def __init__(self, capacity: int):
+        self.dicts = {}
+        self.freq = {}
+        self.capacity = capacity
+        #self.last_used = -1
+        self.lru = {}
+        self.time = 0
+        
+    def get(self, key: int) -> int:
+        self.time+=1
+        if key in self.dicts:
+            #self.last_used = key
+            self.lru[key] = self.time
+            self.freq[key] = self.freq.get(key,0) + 1
+            return self.dicts[key]
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        self.time+=1
+        
+        if self.capacity > 0 or key in self.dicts:
+            self.freq[key] = self.freq.get(key,0) + 1
+            self.lru[key] = self.time
+            if key in self.dicts:
+                self.dicts[key] = value
+                
+            else:
+                self.dicts[key] = value
+                self.capacity-=1
+        
+        elif self.capacity == 0:
+            mins = float('inf')
+            curr_min = -1
+            
+            for x in self.freq:
+                if self.freq[x] < mins:
+                    mins = self.freq[x]
+                    curr_min = x
+                    
+                elif self.freq[x] == mins and self.lru[curr_min] > self.lru[x]:
+                    curr_min = x
+            
+            
+                        
+            if self.lru:
+                self.lru.pop(curr_min)
+                self.dicts.pop(curr_min)
+                self.freq.pop(curr_min)
+                self.dicts[key] = value
+
+                self.freq[key] = 1
+                
+            
+                self.lru[key] = self.time 
+                
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+##Solution2 (Accepted)
 from collections import defaultdict, OrderedDict
 class LFUCache:
 
