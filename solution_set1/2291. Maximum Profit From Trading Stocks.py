b2c3f3404504c4a3e414d2e4e79656f23ee9dfc2
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2022-06-02 20:10:53 -0700

    Update 2291. Maximum Profit From Trading Stocks.py

--- solution_set1/2291. Maximum Profit From Trading Stocks.py
+++ solution_set1/2291. Maximum Profit From Trading Stocks.py
@@ -23,3 +23,23 @@ class Solution:
         
         return find_ans(0,budget)
         
+## Solution 2
+class Solution:
+    def maximumProfit(self, present: List[int], future: List[int], budget: int) -> int:
+        
+        ## a stcok will either be bought or not bought
+        
+        #self.ans = -1
+        
+        dp = [0 for x in range(budget + 1)]
+        
+        for x in range(len(present)):
+            
+            if future[x] - present[x] <=0:
+                continue
+                
+            for y in range(budget, present[x]-1,-1):
+                dp[y] = max(dp[y], dp[y-present[x]] + future[x] - present[x])
+                
+        return dp[-1]
+        
