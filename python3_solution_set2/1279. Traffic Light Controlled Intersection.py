f2cf1e51f60f5c95bb59baaf50872883364c53bf
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-02-12 12:31:50 -0800

    Create 1279. Traffic Light Controlled Intersection.py

---
+++ python3_solution_set2/1279. Traffic Light Controlled Intersection.py
@@ -0,0 +1,18 @@
+class TrafficLight:
+    def __init__(self):
+        self.green = 1
+
+    def carArrived(
+        self,
+        carId: int,                      # ID of the car
+        roadId: int,                     # ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        direction: int,                  # Direction of the car
+        turnGreen: 'Callable[[], None]', # Use turnGreen() to turn light to green on current road
+        crossCar: 'Callable[[], None]'   # Use crossCar() to make car cross the intersection
+    ) -> None:
+
+
+        if self.green!=roadId:
+            turnGreen()
+            self.green = roadId
+        crossCar()
