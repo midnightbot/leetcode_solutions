dba1b682ad38c6908f26604b9cbbcd1f2fd484d9
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-03-03 09:54:29 -0800

    Create 401. Binary Watch.py

---
+++ python3_solution_set2/401. Binary Watch.py
@@ -0,0 +1,4 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+
+        return ['%d:%02d' % (h, m) for h in range(12) for m in range(60) if (bin(h) + bin(m)).count('1') == turnedOn]
