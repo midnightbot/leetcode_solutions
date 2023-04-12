49e77c7124c6e15f1c143d0c46bf668a47920f38
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-04-11 22:47:28 -0700

    Create 2634. Filter Elements from Array.js

---
+++ js/2634. Filter Elements from Array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const ans = [];
+    for(let i=0; i<arr.length;++i){
+        if(fn(arr[i],i)){
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+};
