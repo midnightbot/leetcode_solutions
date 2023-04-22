58a264a294b063ad80ed703acba28eb2b7f7bbe2
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-04-22 11:17:14 -0700

    Create 2649. Nested Array Generator.js

---
+++ js/2649. Nested Array Generator.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function*(arr) {
+    for(let i of arr.flat(Infinity)){
+        yield i;
+    }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
