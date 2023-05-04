9f02bf2956a3948b98a5968e592bab3c07407fbf
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-05-04 14:03:39 -0700

    Create 2666. Allow One Function Call.js

---
+++ js/2666. Allow One Function Call.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let counter = 0;
+    return function(...args){
+        if (counter == 0){
+            counter+=1
+            return fn(...args);
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
