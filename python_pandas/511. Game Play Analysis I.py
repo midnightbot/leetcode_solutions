daf3f272c1a12b60d00cc85c2b1feff8cc607cad
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-08-07 22:38:50 -0700

    Create 511. Game Play Analysis I.py

---
+++ python_pandas/511. Game Play Analysis I.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+
+    return activity.groupby('player_id').agg(first_login=('event_date','min')).reset_index()
+    
