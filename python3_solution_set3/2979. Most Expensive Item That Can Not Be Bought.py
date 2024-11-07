83dcaeef020852671883428c6f04658257bfaf45
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2024-11-06 22:00:18 -0800

    Create 2979. Most Expensive Item That Can Not Be Bought.py

---
+++ python3_solution_set3/2979. Most Expensive Item That Can Not Be Bought.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mostExpensiveItem(self, a: int, b: int) -> int:
+        # ans will be < primeOne * primeTwo
+        # ans will always be a prime number
+        # mcnugget theorem
+
+        return a*b-a-b
+        
+
+
+
+
+        
